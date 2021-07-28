4416dd553d456ee70aea8531d984c98e787c4ee7
jay-benzatine <jayfinava@benzatine.com>
2021-07-28 14:57:11 +0530

    Updates some waterLevel screens and design weight chart and add weight dialog in profile.

--- lib/utils/Utils.dart
+++ lib/utils/Utils.dart
@@ -44,4 +44,38 @@ class Utils {
   static double kgToLb( double weightValue)  {
     return double.parse((weightValue * 2.2046226218488).toStringAsFixed(1));
   }
+
+  static int daysInMonth(final int monthNum, final int year) {
+    List<int> monthLength = List.filled(12, 0, growable: true);
+
+    monthLength[0] = 31;
+    monthLength[2] = 31;
+    monthLength[4] = 31;
+    monthLength[6] = 31;
+    monthLength[7] = 31;
+    monthLength[9] = 31;
+    monthLength[11] = 31;
+    monthLength[3] = 30;
+    monthLength[8] = 30;
+    monthLength[5] = 30;
+    monthLength[10] = 30;
+
+    if (leapYear(year) == true)
+      monthLength[1] = 29;
+    else
+      monthLength[1] = 28;
+
+    return monthLength[monthNum - 1];
+  }
+
+  static bool leapYear(int year) {
+    bool leapYear = false;
+
+    bool leap = ((year % 100 == 0) && (year % 400 != 0));
+    if (leap == true)
+      leapYear = false;
+    else if (year % 4 == 0) leapYear = true;
+
+    return leapYear;
+  }
 }
